bcedd57d7eb0db00d1edd1427629b86e0780ef51
Ramai Alejandria <vyasadev931@gmail.com>
2023-06-01 20:49:32 +0200

    new module

---
+++ backend-services-bank/src/main/java/it/learning/spring/backendservicesbank/BackendServicesBankApplication.java
@@ -0,0 +1,13 @@
+package it.learning.spring.backendservicesbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendServicesBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendServicesBankApplication.class, args);
+    }
+
+}
---
+++ backend-services-bank/src/test/java/it/learning/spring/backendservicesbank/BackendServicesBankApplicationTests.java
@@ -0,0 +1,13 @@
+package it.learning.spring.backendservicesbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendServicesBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
