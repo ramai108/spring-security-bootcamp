8e91c300fe92f7d9558532fd0f12bf76362bf872
Ramai Alejandria <vyasadev931@gmail.com>
2023-06-02 22:43:36 +0200

    further security implementations

---
+++ managing-users/src/main/java/it/learning/spring/managingusers/ManagingUsersApplication.java
@@ -0,0 +1,14 @@
+package it.learning.spring.managingusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ManagingUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagingUsersApplication.class, args);
+    }
+
+}
