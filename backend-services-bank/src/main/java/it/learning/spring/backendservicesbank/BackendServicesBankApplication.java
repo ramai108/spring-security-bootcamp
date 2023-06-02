5fd0a17b5a2dc08dfa0811281033f17995036a41
Ramai Alejandria <vyasadev931@gmail.com>
2023-06-02 10:23:42 +0200

    further security implementations

--- backend-services-bank/src/main/java/it/learning/spring/backendservicesbank/BackendServicesBankApplication.java
+++ backend-services-bank/src/main/java/it/learning/spring/backendservicesbank/BackendServicesBankApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("it.learning.spring.backendservicesbank.controller")
 public class BackendServicesBankApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendServicesBankApplication.class, args);
--- backend-services-bank/src/main/java/it/learning/spring/backendservicesbank/config/ProjectSecurityConfig.java
+++ backend-services-bank/src/main/java/it/learning/spring/backendservicesbank/config/ProjectSecurityConfig.java
@@ -1,9 +1,7 @@
 package it.learning.spring.backendservicesbank.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -12,9 +10,37 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain (HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().anyRequest().authenticated();
-        http.formLogin();
-        http.httpBasic();
+
+        /**
+         * Custom configuration
+         */
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/myAccount","/myBalance","/myLoans","myCards").authenticated()
+                .requestMatchers("/notices","/contact").permitAll()
+                .and().formLogin()
+                .and().httpBasic();
+
+        /**
+         * Custom configuration to deny all requests
+         */
+
+        /*
+            http.authorizeHttpRequests().anyRequest().denyAll()
+                .and().formLogin()
+                .and().httpBasic();
+        */
+
+        /**
+         * Configuration to permit all the requests
+         */
+
+        /*
+          http.authorizeHttpRequests().anyRequest().permitAll()
+             .and().formLogin()
+             .and().httpBasic();
+        */
+
         return http.build();
     }
 
